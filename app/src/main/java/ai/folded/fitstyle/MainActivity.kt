610f85598fcf695fea538a18f3f21aaec4fc547c
Joel Goncalves <goncalves.joel95@gmail.com>
2021-10-24 13:09:46 -0400

    Revert "Initializes amplify on start up"
    This reverts commit a6c775ecae3eb7dfe6ea5140bb795efd2a6349a4.

--- app/src/main/java/ai/folded/fitstyle/MainActivity.kt
+++ app/src/main/java/ai/folded/fitstyle/MainActivity.kt
@@ -5,10 +5,6 @@ import ai.folded.fitstyle.viewmodels.MainViewModel
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil.setContentView
-import com.amplifyframework.AmplifyException
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
-import com.amplifyframework.core.Amplify
-import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,17 +14,7 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        try {
-            Amplify.addPlugin(AWSCognitoAuthPlugin())
-            Amplify.addPlugin(AWSS3StoragePlugin())
-            Amplify.configure(applicationContext)
-
-            viewModel.fetchUser()
-        } catch (failure: AmplifyException) {
-            // TODO: log error
-        }
-
         setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        viewModel.fetchUser()
     }
 }
