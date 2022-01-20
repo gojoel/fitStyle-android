03b61d942d10fc68246247bd4dda90dbdb28fd08
Joel Goncalves <goncalves.joel95@gmail.com>
2022-01-19 19:47:12 -0500

    Extracts photo selection logic to shared utils class

--- app/src/main/java/ai/folded/fitstyle/StyleListFragment.kt
+++ app/src/main/java/ai/folded/fitstyle/StyleListFragment.kt
@@ -4,21 +4,14 @@ import ai.folded.fitstyle.adapters.StyleListAdapter
 import ai.folded.fitstyle.adapters.StyleListener
 import ai.folded.fitstyle.data.StyleOptions
 import ai.folded.fitstyle.databinding.FragmentStyleListBinding
-import ai.folded.fitstyle.utils.PermissionUtils
+import ai.folded.fitstyle.utils.PhotoUtils
 import ai.folded.fitstyle.viewmodels.StyleListViewModel
-import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -30,27 +23,13 @@ class StyleListFragment: Fragment() {
 
     private val styleViewModel: StyleListViewModel by viewModels()
 
-    private val requestPermission =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) launchGallery()
-            else showPermissionDeniedDialog()
-        }
-
-    private val openGallery =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
-                val selectedImageUri: Uri? = result.data?.data
-                if (selectedImageUri != null) {
-                    onPhotoUploaded(selectedImageUri);
-                }
-            }
-        }
+    private val photoUtils = PhotoUtils(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentStyleListBinding.inflate(inflater, container, false)
         context ?: return binding.root
 
@@ -74,7 +53,7 @@ class StyleListFragment: Fragment() {
     }
 
     private fun subscribeUi(adapter: StyleListAdapter) {
-        styleViewModel.images.observe(viewLifecycleOwner, Observer {
+        styleViewModel.images.observe(viewLifecycleOwner, {
             it?.let {
                 binding.shimmerView.visibility = View.GONE
                 binding.styleList.visibility = View.VISIBLE
@@ -89,27 +68,10 @@ class StyleListFragment: Fragment() {
     }
 
     private fun onUploadPhoto() {
-        activity?.let {
-            if (PermissionUtils.isStoragePermissionGranted(it)) {
-                launchGallery()
-            } else {
-                //TODO: check if can request permission
-                requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
+        photoUtils.accessGallery {
+            val options = StyleOptions(customStyleUri = it)
+            onStyleSelected(options)
         }
-    }
-
-    private fun onPhotoUploaded(imageUri: Uri) {
-        val options = StyleOptions(customStyleUri = imageUri)
-        onStyleSelected(options)
-    }
-
-    private fun launchGallery() {
-        openGallery.launch(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
-    }
-
-    private fun showPermissionDeniedDialog() {
-        //TODO: handle this
     }
 
     override fun onResume() {
